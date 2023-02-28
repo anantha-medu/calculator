9a9452fa78ffa76963128628171f9303dc78c516
Anantha Medu <ananthamedu18@gmail.com>
2023-02-28 23:47:40 +0530

    Add functionality to avoid dividing by zero

--- calculator.js
+++ calculator.js
@@ -2,12 +2,14 @@
 
 const numberButtons = Array.from(document.querySelectorAll('.number'));
 const clearButton = document.getElementById("clear");
+const deleteButton = document.getElementById("delete");
 const operationButtons = Array.from(document.querySelectorAll('.operation'));
 const equalsButton = document.getElementById('equals');
+const decimalButton = document.querySelector('.dot');
 
 // to display the numbers on click
 numberButtons.forEach(choice => choice.addEventListener("click", displayNumberOnClick));
-const typedDisplay = document.getElementById("show-typing").firstChild;
+const typedDisplay = document.getElementById("show-typing");
 const upperDisplay = document.getElementById("show-typed");
 
 let firstNumber, secondNumber
@@ -17,9 +19,16 @@ typedDisplay.textContent = typingText;
 // to  display the number when clicked on button
 function displayNumberOnClick(e)
 {
+    
     let num = e.currentTarget.getAttribute("data-number");
     if(typingText === "0")
     {
+        typingText = num;
+    }
+    else if(typedDisplay.classList.contains('finalAnswer'))
+    {
+        upperDisplay.textContent = "";
+        typedDisplay.classList.remove('finalAnswer');
         typingText = num;
     }
     else
@@ -57,6 +66,10 @@ function displayAfterOperationbtnClick(e)
     {
         secondNumber = parseInt(typedDisplay.textContent);
         solution = evaluate(firstNumber, secondNumber, operator);
+        if(isNaN(solution))
+        {
+            return
+        }
         firstNumber = solution;
         operator = e.currentTarget.getAttribute("data-value");
         upperDisplay.textContent = firstNumber + " " + operator;
@@ -72,9 +85,15 @@ equalsButton.addEventListener("click", finalEvaluation);
 function finalEvaluation(e)
 {
     secondNumber = parseInt(typedDisplay.textContent);
-    upperDisplay.textContent = firstNumber + " " + operator + " " + secondNumber
     solution = evaluate(firstNumber, secondNumber, operator);
+    console.log(solution);
+    if(isNaN(solution))
+        {
+            return
+        }
+    upperDisplay.textContent = firstNumber + " " + operator + " " + secondNumber + " " + e.currentTarget.getAttribute("data-value");
     typedDisplay.textContent = solution;
+    typedDisplay.classList.add('finalAnswer');
 }
 
 
@@ -98,15 +117,45 @@ function evaluate(first, second, operator)
             break;
 
         case "/":
-            tempSolution = first / second;
-            break
+            if(second !== 0)
+            {
+                {tempSolution = first / second;}
+                break;
+            }
+            else
+            {   
+                alert("Cannot divide by zero");
+                tempSolution = NaN;
+                break
+            }
     }
 
     return tempSolution;
 }
 
 
+//to delete a digit when button is clicked
 
+deleteButton.addEventListener("click", deleteDigit)
+
+function deleteDigit(e)
+{
+    typingText = typedDisplay.textContent
+    if(typedDisplay.textContent === "" || typedDisplay.textContent === "0")
+    {
+        return;
+    }
+    else if(typedDisplay.textContent.length === 1)
+    {
+        // typedDisplay.textContent = typingText.slice(0, typingText.length-1);
+        typingText = "0";
+        typedDisplay.textContent = "0"; 
+    }
+    else
+    {
+        typedDisplay.textContent = typingText.slice(0, typingText.length-1);
+    }
+}
 
 //to clear all when clear button is clicked
 
