5fb8d12182c96cb22a9fbafde90cbfd4afddabdb
Anantha Medu <ananthamedu18@gmail.com>
2023-02-28 15:44:31 +0530

    Function to display the clicked number

--- calculator.js
+++ calculator.js
@@ -1,25 +1,93 @@
-// to display the numbers on click
+//selecting the elements from html dom
 
 const numberButtons = Array.from(document.querySelectorAll('.number'));
 const clearButton = document.getElementById("clear");
+const operationButtons = Array.from(document.querySelectorAll('.operation'));
 
-
+// to display the numbers on click
 numberButtons.forEach(choice => choice.addEventListener("click", displayNumberOnClick));
 const typedDisplay = document.getElementById("show-typing").firstChild;
+const upperDisplay = document.getElementById("show-typed");
 
 let firstNumber, secondNumber
-let typingText = "";
+let typingText = "0";
 typedDisplay.textContent = typingText;
 
 // to  display the number when clicked on button
 function displayNumberOnClick(e)
 {
     let num = e.currentTarget.getAttribute("data-number");
-    // console.log(typedDisplay);
-    // console.log(num);
-    typingText = typingText + num;
+    if(typingText === "0")
+    {
+        typingText = num;
+    }
+    else
+    {
+        typingText = typingText + num;
+    }
+
     typedDisplay.textContent = typingText;
 }
+
+
+// upper display string content
+
+upperDisplay.textContent = "";
+
+// to display upperstring when any operation keys pressed
+
+operationButtons.forEach(choice => choice.addEventListener("click", displayAfterOperationbtnClick))
+
+function displayAfterOperationbtnClick(e)
+{
+    const operator = e.currentTarget.getAttribute("data-value");
+    
+    if(upperDisplay.textContent === "")
+    {
+        firstNumber = parseInt(typedDisplay.textContent);
+        upperDisplay.textContent = firstNumber + " " + operator
+        typingText = "0";
+    }
+
+    else
+    {
+        secondNumber = parseInt(typedDisplay.textContent);
+        solution = evaluate(firstNumber, secondNumber, operator);
+        firstNumber = solution;
+        upperDisplay.textContent = firstNumber + " " + operator;
+        typingText = "0";
+    }
+}
+
+
+// to evaluate the numbers
+
+function evaluate(first, second, operator)
+{
+    let tempSolution;
+    switch(operator)
+    {
+        case "+":
+            tempSolution = first + second;
+            break;
+        
+        case "-":
+            tempSolution = first - second;
+            break;
+
+        case "*":
+            tempSolution = first * second;
+            break;
+
+        case "/":
+            tempSolution = first / second;
+            break
+    }
+
+    return tempSolution;
+}
+
+
 
 
 //to clear all when clear button is clicked
@@ -30,5 +98,6 @@ function clearAllTyped(e)
 {
     typedDisplay.textContent = "0";
     typingText = "";
+    upperDisplay.textContent = "";
 
 }
